96f2bcd07abc2727b678a192a10a293aabb34219
johnlyden <john.lyden86@gmail.com>
2017-12-13 23:22:38 -0600

    added renderTitleField - mapped all Field.input properties to the input

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+   return (
+    <div>
+      <input
+        type="text"
+        {...field.input} 
+      />
+    </div>
+   ); 
+  }
+  
   render() {
     return (
     <form>
       <Field
         name="title" // what piece of state is this?
-        component={}
+        component={this.renderTitleField}
       />
     </form>
     );
