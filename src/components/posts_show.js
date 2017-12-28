44f03db1cf1f6802ce6a5bf0349f5e52b6310309
johnlyden <john.lyden86@gmail.com>
2017-12-28 16:28:22 -0600

    added postsShow component

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    )
+  }
+}
+
+export default PostsShow;
